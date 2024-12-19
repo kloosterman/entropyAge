0d17ef1723557f3008b93c45c3227614013fe8f9
kloosterman <7921023+kloosterman@users.noreply.github.com>
2024-12-19 14:05:46 +0100

    2-group behavPLS plotting

--- eA_pls.m
+++ eA_pls.m
@@ -2,6 +2,9 @@ addpath('/Users/kloosterman/Documents/GitHub/entropyAge')
 addpath('/Users/kloosterman/Documents/GitHub/plscmd')
 addpath('/Users/kloosterman/Documents/GitHub/fieldtrip_dev')
 addpath('/Users/kloosterman/Documents/GitHub/fieldtrip')
+ft_defaults
+load /Users/kloosterman/Documents/GitHub/plotting-tools/colormap_jetlightgray.mat
+load /Users/kloosterman/Documents/GitHub/LRaudio/Acticap_64_UzL.mat; % lay comes out
 
 datapath = '/Users/kloosterman/projectdata/EntropyAging';
 cd(datapath)
@@ -65,7 +68,7 @@ saveas(f, 'behavPLS_MSE', 'pdf');
 saveas(f, 'behavPLS_MSE', 'png');
 
 %% 2 group PLS
-older_mse_all = young_mse_all;
+older_mse_all = young_mse_all; % @Moritz: put the OA data here!
 older_mse_all.powspctrm = older_mse_all.powspctrm(1:17,:,:,:); % pretend lower N in OA
 
 cfg = [];
@@ -79,4 +82,30 @@ cfg.cormode = 0;                            % 0 is Pearson corr, 8 is Spearman
 cfg.num_cond = 1;                           % Number of conditions
 cfg.design = [behav; behav(1:17,:)];        % append behav OA to YA
 cfg.num_subj_lst = [19 17];                 % Number of subjects per condition, array!
-stat_mse = ft_freqstatistics(cfg, young_mse_all, older_mse_all);
+stat_mse_2group = ft_freqstatistics(cfg, young_mse_all, older_mse_all);
+
+% plot 2 group PLS: 1 BSR map for LV1, but two sets of bars: for YA and OA:
+% Goal: see if we get same direction of bars for YA and OA
+f = figure; f.Position = [680         624        1171         254];
+tiledlayout(1,5);
+cfg.clussign = 'pos';
+cfg.clus2plot = 1;   cfg.integratetype = 'trapz'; % mean or trapz
+stat_mse.posclusterslabelmat = stat_mse.mask;
+ft_clusterplot3D(cfg, stat_mse)
+
+% plot latent behav vs latent brain
+nexttile; s = scatter(stat_mse.behavscores, stat_mse.brainscores, 'MarkerEdgeColor',[1 1 1],  'MarkerFaceColor', [0 0 0], 'LineWidth',1.0, 'SizeData', 40); axis padded; lsline; box on;
+xlabel('"Cognitive rigidness"'); ylabel('EEG entropy at rest'); 
+title(sprintf('r = %1.2f', corr(stat_mse.brainscores, stat_mse.behavscores)))
+
+% bar plot corrs for each behav var
+nexttile; b=bar(stat_mse.results.lvcorrs(1:11,1)); xticklabels(behavNames); % Assuming 11 behavioral variables!
+ylabel('Correlation')
+title('Brain score vs behavior YA')
+
+nexttile; b=bar(stat_mse.results.lvcorrs(12:end,1)); xticklabels(behavNames); % Assuming 11 behavioral variables!
+ylabel('Correlation')
+title('Brain score vs behavior OA')
+
+saveas(f, 'behavPLS_MSE_2group', 'pdf');
+saveas(f, 'behavPLS_MSE_2group', 'png');
